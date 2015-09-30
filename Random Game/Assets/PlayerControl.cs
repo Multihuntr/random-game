50d31ce13f1e5c5c431ff136705853dc0c4d756f
Multihuntr <Multihuntr@hotmail.com>
2015-10-01 02:01:10 +1000

    Removed 'Main' scene because it smells; replaced with my own version.
    Replaced with a "testing" scene. Thanks to Harry, we figured out why the
    2D colliders weren't playing nice.
    Added back stuff from 'Main' and extended. You can now control a cube.
    The movement is reasonably smooth, but the player will often move
    part-way into an object before being moved back. Additionally, due to
    the way that the velocity interacts with the physics engine, you can
    hold onto the wall indefinitely.
    
    Regardless, the basics of movement are there, we can tweak the numbers
    later.

---
+++ Random Game/Assets/PlayerControl.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    Rigidbody2D rb;
+    Mesh mesh;
+
+    public float runSpd;
+    public float runSmoothing;
+    public float jumpHeight;
+    public float jumpTriggerHeight;
+
+    private bool jumpHeld;
+    
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        mesh = GetComponent<MeshFilter>().mesh;
+        jumpTriggerHeight += mesh.bounds.size.y;
+	}
+
+    bool isGrounded()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, jumpTriggerHeight, 1 << 8);
+    }
+	
+	void LateUpdate () {
+        float targXVel = Input.GetAxis("Horizontal") != 0 ? Input.GetAxis("Horizontal")*runSpd : 0;
+        float targYVel = rb.velocity.y;
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            if (isGrounded() && !jumpHeld)
+            {
+                targYVel = jumpHeight;
+            }
+            jumpHeld = true;
+        } else
+        {
+            jumpHeld = false;
+        }
+        rb.velocity = Vector2.Lerp(rb.velocity, new Vector2(targXVel, targYVel), runSmoothing);
+    }
+}
