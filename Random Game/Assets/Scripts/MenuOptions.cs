22daaa626a341decd5d9bdd6d1aa9ee35b227c56
Iain McManus <level8mudcrab@gmail.com>
2015-11-25 15:11:14 +1100

    Start Menu
    First prototype for the start menu. Continue Game doesn't do anything at
    the moment and start game will have to be linked up properly when actual
    levels are made.

---
+++ Random Game/Assets/Scripts/MenuOptions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MenuOptions : MonoBehaviour
+{
+
+    //Takes an int representing the index of the scene in the build settings
+    public void LoadScene(int level)
+    {
+        Application.LoadLevel(level);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
