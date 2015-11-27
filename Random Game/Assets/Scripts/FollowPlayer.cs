6579c29d641df5119fed77117dfe2e146eb23a5f
Multihuntr <Multihuntr@hotmail.com>
2015-11-27 14:54:57 +1100

    Naive Camera Follow Script made
    Along with a level to go and "explore"

---
+++ Random Game/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+	public GameObject player;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = new Vector3 (player.transform.position.x
+		                                  , player.transform.position.y
+		                                  , transform.position.z);
+	}
+}
