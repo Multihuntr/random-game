b274fc24ff3244e3545034389ab9a2c55c3bb316
Multihuntr <Multihuntr@hotmail.com>
2015-11-27 12:14:27 +1100

    Implemented health value
    Also a small resize on HP bar

---
+++ Random Game/Assets/Scripts/Health.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Health : MonoBehaviour
+{
+	
+	public int maxHealth;
+	protected int currHealth;
+	
+	abstract protected void takeDamage (int amount);
+	
+	protected void Init ()
+	{
+		currHealth = maxHealth;
+	}
+}
