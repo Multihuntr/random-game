50223a234767856931ded59a0ab0afa736b0acc8
Multihuntr <Multihuntr@hotmail.com>
2015-11-30 18:04:43 +1100

    Spawn function is called appropriately
    But it just debug.logs a message.

---
+++ Random Game/Assets/Scripts/Enemies/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+	public GameObject toSpawn;
+	public float distToSpawn;
+
+	GameObject spawned;
+
+	void Start ()
+	{
+		InvokeRepeating ("checkSpawn", 0, 1.0f);
+	}
+
+	void checkSpawn ()
+	{
+		Vector3 distFromView = Camera.main.WorldToViewportPoint (transform.position);
+		if (Mathf.Abs (distFromView.x) > distToSpawn || Mathf.Abs (distFromView.y) > distToSpawn) {
+			spawn ();
+		}
+	}
+
+	void spawn ()
+	{
+		Debug.Log ("Spawned");
+	}
+}
