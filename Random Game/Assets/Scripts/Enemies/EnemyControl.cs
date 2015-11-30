9713799b41395f4e72388c846c2ae383026cefe9
Multihuntr <Multihuntr@hotmail.com>
2015-12-01 09:58:43 +1100

    Enemy scuttles!
    Beware the small green thing with no discernable features

--- Random Game/Assets/Scripts/Enemies/EnemyControl.cs
+++ Random Game/Assets/Scripts/Enemies/EnemyControl.cs
@@ -8,25 +8,13 @@ public class EnemyControl : EntityControl
 	void OnTriggerEnter2D (Collider2D other)
 	{
 		if (other.gameObject.tag == "Player") {
-			other.GetComponent<PlayerHealth> ().takeDamage (1000);
+			other.GetComponent<PlayerHealth> ().takeDamage (damage);
 		}
-	}
-
-	void Start ()
-	{
-		// Sort of hacky way of determining the width and height in this case
-		Transform child = transform.GetChild (0);
-		Mesh mesh = GetComponentInChildren<MeshFilter> ().mesh;
-		width = mesh.bounds.size.x * child.localScale.x;
-		height = mesh.bounds.size.y * child.localScale.y;
 	}
 
 	void Update ()
 	{
 		yVel = newYVel (yVel);
-		float x = transform.position.x;
-		float y = transform.position.y;
-		float z = transform.position.z;
-		transform.position = new Vector3 (x, y + yVel * Time.deltaTime, z);
+		updatePos (0, yVel);
 	}
 }
